963cfde298f29b016f6ae1859dc220ea804f0f0c
Anthony M Powers <tehraven@gmail.com>
2021-11-28 09:56:21 -0800

    Show main character too

--- src/resources/views/map.blade.php
+++ src/resources/views/map.blade.php
@@ -14,7 +14,7 @@
         <a href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(),
         array_merge(request()->route()->parameters, ['character' => $character])) }}">
           {!! img('characters', 'portrait', $character->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-          {{ $character->name }}
+          {{ $character->name }} ({{ $character->user->main_character->name }})
         </a>
 
         <span class="id-to-name text-muted float-right">@include('web::partials.location', ['location' => $character->location])</span>
