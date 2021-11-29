80f72ad7bb777b023fad32a568a1a1366cc5cb55
Anthony M Powers <tehraven@gmail.com>
2021-11-29 14:27:21 -0800

    Blade refs

---
+++ src/resources/views/left.blade.php
@@ -0,0 +1,33 @@
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Characters</h3>
+    </div>
+    <div class="card-body">
+        <h4>Regions</h4>
+        <div class="form-group">
+          <select name="regions" id="region_list" class="form-control">
+              @foreach($allRegions as $aRegion)
+                  <option value="{{ $aRegion->region_id }}"  @if($region && $aRegion->region_id === $region->region_id) selected @endif>{{ $aRegion->name }}</option>
+              @endforeach
+          </select>
+        </div>
+        
+        <h4>Characters</h4>
+        <div class="form-group">
+          <ul class="list-group list-group-unbordered mb-3">
+              @foreach($characters as $character)
+                  <li class="list-group-item">
+
+                      <a href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(),
+                          array_merge(request()->route()->parameters, ['character' => $character])) }}">
+                      {!! img('characters', 'portrait', $character->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+                      {{ $character->name }} ({{ $character->user->main_character->name }})
+                      </a>
+
+                      <span class="id-to-name text-muted float-right">@include('web::partials.location', ['location' => $character->location])</span>
+                  </li>
+              @endforeach
+          </ul>
+        </div>
+    </div>
+</div>
