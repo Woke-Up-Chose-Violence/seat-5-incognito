430949acbcaff7b441a0c756330f35c3393741ce
Anthony M Powers <tehraven@gmail.com>
2021-12-01 12:39:31 -0800

    Fix array_merge and icons

--- src/Config/characterlocationmap.sidebar.php
+++ src/Config/characterlocationmap.sidebar.php
@@ -28,7 +28,7 @@ return [
                 'name' => 'character-map',
                 'label' => 'characterlocationmap::menu.main_level',
                 'plural' => false,
-                'icon' => 'fas fa-map-marker-check',
+                'icon' => 'fas fa-map-marker',
                 'route_segment' => 'characterlocationmap',
                 'route' => 'characterlocationmap.home',
             ],
