31618b16b466a9314bf51a878e582f649bcca7f5
Anthony M Powers <tehraven@gmail.com>
2021-11-30 18:19:00 -0800

    Fix array key exists

--- src/CharacterLocationMapServiceProvider.php
+++ src/CharacterLocationMapServiceProvider.php
@@ -218,7 +218,7 @@ class CharacterLocationMapServiceProvider extends AbstractSeatPlugin
                 continue;
             }
 
-            if (!Arr::exists($merging, $key)) {
+            if (!array_key_exists($key, $merging)) {
                 continue;
             }
 
