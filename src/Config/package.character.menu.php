2ac3a7d80d76dfcc6600a9b364028535544e53c1
Raven Dubh <tehraven@gmail.com>
2024-07-02 15:20:01 -0700

    Sidebar fixes

---
+++ src/Config/package.character.menu.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    [
+        'name' => 'character-map',
+        'label' => 'characterlocationmap::menu.main_level',
+        'plural' => false,
+        'icon' => 'fas fa-map-marker',
+        'route_segment' => 'characterlocationmap',
+        'route' => 'characterlocationmap.home',
+    ],
+];
