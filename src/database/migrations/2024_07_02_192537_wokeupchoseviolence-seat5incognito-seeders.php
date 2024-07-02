4211a170f3bb7f972b7fd6a74ec6187ba2092a9b
Raven Dubh <tehraven@gmail.com>
2024-07-02 12:46:59 -0700

    Seeders are now part of migrations

--- src/database/migrations/2024_07_02_192537_wokeupchoseviolence-seat5incognito-seeders.php
+++ src/database/migrations/2024_07_02_192537_wokeupchoseviolence-seat5incognito-seeders.php
@@ -10,7 +10,7 @@ return new class extends Migration
     // public static $CORP_URHI = 98427836;
     // public static $CORP_VPN = 98491871;
     
-    public static $SEEDERS = [
+    public static $seeders = [
         [   
             'command' => 'bomb:corp ' . 98491871,
             'expression' => '0 2 * * *',
